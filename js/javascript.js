7aa8b5c28edb7e3dc56b7d8dc842560b116cbc36
jesse emerson <mrloquacious@gmail.com>
2018-09-11 13:53:53 -0700

    Not sure, it's been awhile ...

--- js/javascript.js
+++ js/javascript.js
@@ -1,47 +1,3 @@
-// ***I set the hover function actions in CSS.
-//$(document).ready(function() {
-  //  $('#get-started-button').hover(
-    //    function() {
-      //      $(this).toggleClass('hover')
-        //});
-//});
-//$(document).ready(function() {
-  //  $('#login-button').hover(
-    //    function() {
-      //      $(this).toggleClass('hover')
-        //});
-//});
-
-// How can I open these in new tabs?:
-
-//Did these ever work?  At any rate, some of these are obsolete now, I think?:
-/*$(document).ready(function() {
-    $('#get-started-button').click(
-        function() {
-            window.location = "http://www.markortonmusic.com"
-        });
-});
-
-$(document).ready(function() {
-    $('#go-button').click(
-        function() {
-            window.location = "http://www.markortonmusic.com"
-        });
-});
-
-$(document).ready(function() {
-    $('#password-retrieval-button').click(
-        function() {
-            window.location = "forgot_password.html"
-        });
-});
-
-$(document).ready(function() {
-    $('#account-creation-button').click(
-        function() {
-            window.location = "http://www.markortonmusic.com"
-        });
-});*/
 
 function checkThis() {
     var numberChecked = document.querySelectorAll('input:checked').length;
@@ -71,12 +27,14 @@ function checkThis() {
     {
       document.getElementById('your-score-number').innerHTML = 4
     }
+
+    document.write(numberChecked);
 }
 
 /* Change this for st4a_score: */
 $(document).ready(function(){
 /* Keep this.  It's used in the js.cookies plug in: */
-/*  var cbox_bool = Cookies.get("cbox_bool") */
+var cbox_bool = Cookies.get("cbox_bool")
 
   var st4a_score = $.cookie("st4a_score")
 
@@ -89,32 +47,9 @@ $(document).ready(function(){
   };
 });
 
-
-
-
 $( ":checkbox" )
   .map(function() {
     return this.id;
   })
   .get()
   .join();
-
-//$(document).ready(function(){
-
-//    var searchIDs = $('input:checked').map(function(){
-
-//      return $(this).val();
-
-//    });
-//    console.log(searchIDs.get());
-
-//});
-
-
-// Is this necessary, or is it easier to use an <a> in html?:
-//$(document).ready(function() {
-  //  $('#next-button').click(
-    //    function() {
-      //      window.location = "what_is_meant.html"
-        //});
-//});
